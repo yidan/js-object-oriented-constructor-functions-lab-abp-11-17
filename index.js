ac5bca979c58fa1096b1456f9c746dbf06bad476
Yidan Wang <wangyidan621@gmail.com>
2017-11-12 18:05:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function Scooter(year, color,model){
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+
+function Driver(name, age, experience){
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
